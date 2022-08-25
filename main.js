06ca4067e7679baac47135571488e39020761ef0
YashCODER1 <94070528+YashCODER1@users.noreply.github.com>
2022-08-25 22:54:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+img="";
+status="";
+objects=[];
+
+function setup()
+{
+    canvas=createCanvas(640 , 420);
+    canvas.center();
+
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error("error");
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
+function preload()
+{
+    img=loadImage("loin and deer image.jpg");
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    if(status != "")
+    {
+        for(i = 0; i < objects.length;i++)
+        {
+            document.getElementById("status").innerHTML="Status : Object Detected ";
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+percent+"%",objects[i].x+15,objects[i].y+15);
+            nofill();
+            stroke("#ff0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
